084af756dbc55397d62fcd8db8ead36ca941c108
alvickyfun <Ariel@192.168.1.116>
2019-07-16 21:34:29 -0500

    t h i s  o n e  i s  4  y o u  d a d

--- CS200/P7/Challenge1.java
+++ CS200/P7/Challenge1.java
@@ -7,6 +7,7 @@
 public class Challenge1 {
 
 	public static void main(String[] args) {
+		// print out the sum of the digits for every number from 2-100,000
 		for (int i = 1; i <= 100000; i++) {
 			System.out.println("The sum of the digits of " + i + " is: " + computeDigitSum(i));
 		}
@@ -14,10 +15,22 @@ public class Challenge1 {
 	}
 
 	public static int computeDigitSum(int n) {
+		/*
+		 * start by finding the remainder after dividing by 10 aka the first digit of
+		 * "n"
+		 */
 		int sum = n % 10;
 		if (n < 10) {
+			/*
+			 * if "n" is less than 10, return that number there is no need to add anything,
+			 * for obvious reasons :))
+			 */
 			return n;
 		} else {
+			/*
+			 * return the digit of the ones, and compute the digit of the tens then the
+			 * hundreds, until it n becomes less than 10
+			 */
 			return sum + computeDigitSum(n / 10);
 		}
 	}

e75e5aa25f786822cb270d65d9407f4e3a5a8c50
alvickyfun <Ariel@192.168.1.116>
2019-07-22 15:18:29 -0500

    f i x e d  d a d!

--- CS200/P8/src/Parade.java
+++ CS200/P8/src/Parade.java
@@ -6,38 +6,90 @@
 public class Parade {
 
 	public static void main(String[] args) {
-		/*
-		 * int[] test1 = { 4, 5, 2, 5, 4 }; int[] test2 = { 1, 5, 10, 1 }; int[]
-		 * test4 = new int[] { 1, 10, 22, 40, 100, 40000, 40001 }; int[] test3 =
-		 * new int[] { 1, 1, 1, 1 }; System.out.println("3 --> " +
-		 * findBestFloat(test1)); System.out.println("2 --> " +
-		 * findBestFloat(test2)); System.out.println("5 --> " +
-		 * findBestFloat(test4)); System.out.println("0" + " --> " +
-		 * findBestFloat(test3));
-		 * 
-		 * 
-		 * int[] test3 = { 500, 100, 200, 300, 200, 4, 3, 2, 1, 500, 200, 300,
-		 * 100, 201 }; System.out.println("11 --> " + findTallestGroup(test3));
-		 * 
-		 * int[] test1 = { 10, 5, 6, 7, 8, 11 }; findNovelFloats(test1);
-		 * System.out.println("----------------------"); int[] test2 = { 1, 2,
-		 * 3, 4, 5, 5 }; findNovelFloats(test2);
-		 * System.out.println("----------------------"); int[] test3 = { 10, 9,
-		 * 10 }; findNovelFloats(test3);
-		 * System.out.println("----------------------"); int[] test4 = { 2, 5,
-		 * 4, 8, 6, 7, 8, 10}; findNovelFloats(test4);
-		 * 
-		 * int[] test1 = { 1, 2, 5, 4, 6, 4, 7, 8, 9, 10, 11, 12 }; int[] far =
-		 * findLongestIncreasingSequence(test1); printArray(far);
-		 * 
-		 * int[] test2 = {10,9,8,7,6,5,4,3,2,1}; int[] far1 =
-		 * findLongestIncreasingSequence(test2); printArray(far1); int[] test1 =
-		 * {0,0,0,0,0,0,0,0,0,0,0,0}; System.out.println("2 --> " +
-		 * findTallestGroup(test1));
-		 */
-		
-		int[] test13 = {0,0,0};
+/*
+		int[] test1 = { 4, 5, 2, 5, 4 };
+		int[] test2 = { 1, 5, 10, 1 };
+		int[] test4 = new int[] { 1, 10, 22, 40, 100, 40000, 40001 };
+		int[] test3 = new int[] { 1, 1, 1, 1 };
+		System.out.println("3 --> " + findBestFloat(test1));
+		System.out.println("2 --> " + findBestFloat(test2));
+		System.out.println("5 --> " + findBestFloat(test4));
+		System.out.println("0" + " --> " + findBestFloat(test3));
+		System.out.println("End of findBestFloat testing -----------------------");
+		System.out.println("");
+
+		int[] test3B = { 500, 100, 200, 300, 200, 4, 3, 2, 1, 500, 200, 300, 100, 201 };
+		System.out.println("11 --> " + findTallestGroup(test3B));
+		int[] test4B = {0,0,0,0,0,0,0};
+		System.out.println("2 --> " + findTallestGroup(test4B));
+		int[] test5B = {1,1,1,1,1,1};
+		System.out.println("2 --> "+findTallestGroup(test5B));
+		System.out.println("End of findTallestGroup testing -----------------------");
+		System.out.println("");
+
+		//base cases from prof
+		int[] test1A = { 10, 5, 6, 7, 8, 11 };
+		findNovelFloats(test1A);
+		System.out.println("");
+		int[] test2A = { 1, 2, 3, 4, 5, 5 };
+		findNovelFloats(test2A);
+		System.out.println("");
+		int[] test3A = { 10, 9, 10 };
+		findNovelFloats(test3A);
+		System.out.println("");
+		int[] test4A = { 2, 5, 4, 8, 6, 7, 8, 10 };
+		findNovelFloats(test4A);
+		System.out.println("");
+		int[] test13 = { 0, 0, 0 };
 		findNovelFloats(test13);
+		System.out.println("");
+		//all increasing
+		int[] test5A = {1,2,3,4,5,6,7};
+		findNovelFloats(test5A);
+		System.out.println("");
+		//all decreasing
+		int[] test6a = {7,6,5,4,3,2,1,0};
+		findNovelFloats(test6a);
+		System.out.println("");
+		//increasing at beginning
+		int[] test7a = {1,5,9,12,10,8,2};
+		findNovelFloats(test7a);
+		System.out.println("");
+		//increasing at end
+		int[] test8a = {5,4,2,6,9,84,259};
+		findNovelFloats(test8a);
+		
+		System.out.println("End of findNovelFloats testing -----------------------");
+		System.out.println("");
+*/
+		int[] test1C = { 1, 2, 5, 4, 6, 4, 7, 8, 9, 10, 11, 12 };
+		int[] far = findLongestIncreasingSequence(test1C);
+		printArray(far);
+		System.out.println();
+		int[] test2C = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		int[] far1 = findLongestIncreasingSequence(test2C);
+		printArray(far1);
+		System.out.println();
+		int[] test1D = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		int[] fa4 = findLongestIncreasingSequence(test1D);
+		System.out.println();
+		int[] test1E = {1,2,3,1,2,3,1,2,3};
+		int[] far2 = findLongestIncreasingSequence(test1E);
+		printArray(far2);
+		System.out.println();
+		int[] test23 = {1,2,3,4,8,9,24};
+		int[] ew = findLongestIncreasingSequence(test23);
+		printArray(ew);
+		System.out.println();
+		int[] test32 = {1,2,3,4,8,9,2,1};
+		int[] ewwdf = findLongestIncreasingSequence(test32);
+		printArray(ewwdf);
+		System.out.println();
+		int[] test239 = {5,3,2,1,9,81,259};
+		int[] ewlfedk = findLongestIncreasingSequence(test239);
+		printArray(ewlfedk);
+		System.out.println();
+		System.out.println("End of findTallestGroup testing -----------------------");
 		
 	}
 
@@ -108,7 +160,7 @@ public class Parade {
 		int tallestHeightSoFar = -1;
 		// run through every elements to find which element is taller than all
 		// the floats that came before it
-		
+
 		for (int i = 0; i < heights.length; i++) {
 			// if the tallest float so far is less than the current height set
 			// the tallest

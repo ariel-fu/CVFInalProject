44abbe22c45a61f1e1703c4102376c6b2388f643
alvickyfun <Ariel@192.168.1.116>
2019-07-31 20:06:12 -0500

    P10-new

--- CS200/P10/src/DefinitelyNotACalculator.java
+++ CS200/P10/src/DefinitelyNotACalculator.java
@@ -1,3 +1,4 @@
+
 /*
  * @author: Ariel Fu
  * @studentID:  908 168 5910
@@ -6,17 +7,10 @@
 
 import java.util.Scanner;
 
-//TO ASK: 
-//- what to return
-//- divide by 0, is infinity or NaN okay? 
-//- sqrt of negative numbers 
-//- log of 0 or negative numbers
-//- sum of extremely large numbers i.e. 10E100 10E1000 10E1000000
-//- can array print out with brackets and commas?
 public class DefinitelyNotACalculator {
 
 	public static String PLUS = "+";
-	public static String SUBSTRACT = "-";
+	public static String SUBTRACT = "-";
 	public static String MULTIPLY = "*";
 	public static String DIVIDE = "/";
 	public static String MAX = "max";
@@ -29,6 +23,7 @@ public class DefinitelyNotACalculator {
 	public static String SORT = "sort";
 
 	public static void main(String[] args) {
+		System.out.println("Good day, Mr. Finch! Welcome to your totally-not-a-calculator.");
 
 		Scanner sc = new Scanner(System.in);
 		String stringInputed = "";
@@ -36,14 +31,31 @@ public class DefinitelyNotACalculator {
 
 			System.out.println("Please input an expression: ");
 			stringInputed = sc.nextLine();
-			stringInputed.toLowerCase();
+			stringInputed.trim();
 			String[] splitString = stringInputed.split(" ");
-
+			
 			String operator = splitString[0];
-
+			operator.toLowerCase();
+			if (operator.equalsIgnoreCase("quit")) {
+				break;
+			}
+			if (!isValidOperator(operator)) {
+				System.out.println("Error! Command not found.");
+				continue;
+			}
+			if (!isValidArgument(splitString)) {
+				System.out.println("Error! Incorrect amount of arguments");
+				continue;
+			}
+			if (operator.equalsIgnoreCase(SORT)) {
+				sort(splitString);
+				System.out.println();
+			} else {
+				System.out.println(calculate(splitString));
+			}
 			// TODO
 
-		} while (!stringInputed.equals("quit"));
+		} while (!stringInputed.equalsIgnoreCase("quit"));
 
 		System.out.println("Have a nice day, Mr. Finch!");
 		sc.close();
@@ -53,27 +65,27 @@ public class DefinitelyNotACalculator {
 	public static double calculate(String[] splitString) {
 
 		String operator = splitString[0];
-		if (operator.equals(PLUS)) {
+		if (operator.equalsIgnoreCase(PLUS)) {
 			return addNumber(splitString);
-		} else if (operator.equals(SUBSTRACT)) {
+		} else if (operator.equalsIgnoreCase(SUBTRACT)) {
 			return subtract(splitString);
-		} else if (operator.equals(MULTIPLY)) {
+		} else if (operator.equalsIgnoreCase(MULTIPLY)) {
 			return multiply(splitString);
-		} else if (operator.equals(DIVIDE)) {
+		} else if (operator.equalsIgnoreCase(DIVIDE)) {
 			return divide(splitString);
-		} else if (operator.equals(MAX)) {
+		} else if (operator.equalsIgnoreCase(MAX)) {
 			return max(splitString);
-		} else if (operator.equals(ABS)) {
+		} else if (operator.equalsIgnoreCase(ABS)) {
 			return abs(splitString);
-		} else if (operator.equals(LOG)) {
+		} else if (operator.equalsIgnoreCase(LOG)) {
 			return log(splitString);
-		} else if (operator.equals(POW)) {
+		} else if (operator.equalsIgnoreCase(POW)) {
 			return pow(splitString);
-		} else if (operator.equals(SQRT)) {
+		} else if (operator.equalsIgnoreCase(SQRT)) {
 			return sqrt(splitString);
-		} else if (operator.equals(RAND)) {
+		} else if (operator.equalsIgnoreCase(RAND)) {
 			return rand(splitString);
-		} else if (operator.equals(SUM)) {
+		} else if (operator.equalsIgnoreCase(SUM)) {
 			return sum(splitString);
 		}
 		return 0;
@@ -83,29 +95,29 @@ public class DefinitelyNotACalculator {
 	public static boolean isValidArgument(String[] splitString) {
 		String operator = splitString[0];
 
-		if (operator.equals(PLUS)) {
+		if (operator.equalsIgnoreCase(PLUS)) {
 			return isTwoNumbers(splitString);
-		} else if (operator.equals(SUBSTRACT)) {
+		} else if (operator.equalsIgnoreCase(SUBTRACT)) {
 			return isTwoNumbers(splitString);
-		} else if (operator.equals(MULTIPLY)) {
+		} else if (operator.equalsIgnoreCase(MULTIPLY)) {
 			return isTwoNumbers(splitString);
-		} else if (operator.equals(DIVIDE)) {
+		} else if (operator.equalsIgnoreCase(DIVIDE)) {
 			return isTwoNumbers(splitString);
-		} else if (operator.equals(MAX)) {
+		} else if (operator.equalsIgnoreCase(MAX)) {
 			return isTwoNumbers(splitString);
-		} else if (operator.equals(ABS)) {
+		} else if (operator.equalsIgnoreCase(ABS)) {
 			return isOneNumber(splitString);
-		} else if (operator.equals(LOG)) {
+		} else if (operator.equalsIgnoreCase(LOG)) {
 			return isOneNumber(splitString);
-		} else if (operator.equals(POW)) {
+		} else if (operator.equalsIgnoreCase(POW)) {
 			return isTwoNumbers(splitString);
-		} else if (operator.equals(SQRT)) {
+		} else if (operator.equalsIgnoreCase(SQRT)) {
 			return isOneNumber(splitString);
-		} else if (operator.equals(RAND)) {
+		} else if (operator.equalsIgnoreCase(RAND)) {
 			return isValidRandArgument(splitString);
-		} else if (operator.equals(SUM)) {
+		} else if (operator.equalsIgnoreCase(SUM)) {
 			return isValidEndlessArgument(splitString);
-		} else if (operator.equals(SORT)) {
+		} else if (operator.equalsIgnoreCase(SORT)) {
 			return isValidEndlessArgument(splitString);
 		}
 
@@ -118,82 +130,131 @@ public class DefinitelyNotACalculator {
 	}
 
 	public static boolean isValidRandArgument(String[] splitString) {
-		return true;
+		if (isOneNumber(splitString) || isTwoNumbers(splitString)) {
+			if (isTwoNumbers(splitString)) {
+				double var1 = 0;
+				double var2 = 0;
+				double[] result = new double[1];
+				if (isNumeric(splitString[1], result)) {
+					var1 = result[0];
+				}
+				if (isNumeric(splitString[2], result)) {
+					var2 = result[0];
+				}
+				if (isInt(var1) && isInt(var2)) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+			
+			if(isOneNumber(splitString)) {
+			return true;
+			}
+		}
+
+		return false;
+
 	}
 
 	public static boolean isInt(double val) {
-		return true;
+		int test = (int) val;
+		return test == val;
+
 	}
 
 	// two numbers checks if the length is three and if both the doubles are numeric
 	// or not
 	public static boolean isTwoNumbers(String[] splitString) {
+		int numWhiteSpaces=0;
+		if(isWhiteSpace(splitString)) {
+			numWhiteSpaces++;
+		}
+		if ((splitString.length-numWhiteSpaces) != 3) {
+			return false;
+		}
+		double[] test = new double[1];
+		if (!isNumeric(splitString[1], test)) {
+			return false;
+		}
+		if (!isNumeric(splitString[2], test)) {
+			return false;
+		}
 		return true;
 	}
 
 //one number checks if the length is two, and if the double is numeric	
 	public static boolean isOneNumber(String[] splitString) {
+	
+		if (splitString.length!= 2) {
+			return false;
+		}
+		double[] test = new double[1];
+		if (!isNumeric(splitString[1], test)) {
+			return false;
+		}
 		return true;
+
 	}
 
 	public static boolean isValidOperator(String operator) {
 		// +
-		if (operator.equals(PLUS)) {
+		if (operator.equalsIgnoreCase(PLUS)) {
 			return true;
 		}
 
 		// -
-		if (operator.equals(SUBSTRACT)) {
+		if (operator.equalsIgnoreCase(SUBTRACT)) {
 			return true;
 		}
 
 		// *
-		if (operator.equals(MULTIPLY)) {
+		if (operator.equalsIgnoreCase(MULTIPLY)) {
 			return true;
 		}
 
 		// /
-		if (operator.equals(DIVIDE)) {
+		if (operator.equalsIgnoreCase(DIVIDE)) {
 			return true;
 		}
 
 		// max
-		if (operator.equals(MAX)) {
+		if (operator.equalsIgnoreCase(MAX)) {
 			return true;
 		}
 
 		// log
-		if (operator.equals(LOG)) {
+		if (operator.equalsIgnoreCase(LOG)) {
 			return true;
 		}
 
 		// abs
-		if (operator.equals(ABS)) {
+		if (operator.equalsIgnoreCase(ABS)) {
 			return true;
 		}
 
 		// pow
-		if (operator.equals(POW)) {
+		if (operator.equalsIgnoreCase(POW)) {
 			return true;
 		}
 
 		// sqrt
-		if (operator.equals(SQRT)) {
+		if (operator.equalsIgnoreCase(SQRT)) {
 			return true;
 		}
 
 		// rand
-		if (operator.equals(RAND)) {
+		if (operator.equalsIgnoreCase(RAND)) {
 			return true;
 		}
 
 		// sum
-		if (operator.equals(SUM)) {
+		if (operator.equalsIgnoreCase(SUM)) {
 			return true;
 		}
 
 		// sort
-		if (operator.equals(SORT)) {
+		if (operator.equalsIgnoreCase(SORT)) {
 			return true;
 		}
 
@@ -250,59 +311,177 @@ public class DefinitelyNotACalculator {
 
 	// if it is a one number input, use the randOne, and same for two number input
 	public static double rand(String[] splitString) {
+		if (isOneNumber(splitString)) {
+			return randOne(splitString);
+		}
+		if (isTwoNumbers(splitString)) {
+			return randTwo(splitString);
+		}
 		return 0;
 	}
 
 	// shifts Math.random by val1
 
 	public static double randOne(String[] splitString) {
-		return 0;
+		double var1 = 0;
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+
+		return var1 * Math.random();
 	}
 
 	// shift Math.Random by the difference between the max and the min then add the
 	// min
 	public static int randTwo(String[] splitString) {
+		double var1 = 0;
+		double var2 = 0;
+		double[] result = new double[1];
+		if (isInt(var1) && isInt(var2)) {
+			if (isNumeric(splitString[1], result)) {
+				var1 = result[0];
+			}
+			if (isNumeric(splitString[2], result)) {
+				var2 = result[0];
+			}
+			return (int) ((var2 - var1+1) * Math.random() + var1);
+		}
+
 		return 0;
 	}
 
 	public static double sqrt(String[] splitString) {
-		return 0;
+		double var1 = 0;
+
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		return Math.sqrt(var1);
 	}
 
 	public static double pow(String[] splitString) {
-		return 0;
+		double var1 = 0;
+		double var2 = 0;
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		if (isNumeric(splitString[2], result)) {
+			var2 = result[0];
+		}
+		return Math.pow(var1, var2);
 	}
 
 	public static double abs(String[] splitString) {
-		return 0;
+		double var1 = 0;
+
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		return Math.abs(var1);
 	}
 
 	public static double log(String[] splitString) {
-		return 0;
+		double var1 = 0;
+
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		return Math.log10(var1);
 	}
 
 	public static double max(String[] splitString) {
-		return 0;
+		double var1 = 0;
+		double var2 = 0;
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		if (isNumeric(splitString[2], result)) {
+			var2 = result[0];
+		}
+		if (var1 >= var2) {
+			return var1;
+		}
+		return var2;
 	}
 
 	public static double divide(String[] splitString) {
-		return 0;
+		double var1 = 0;
+		double var2 = 0;
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		if (isNumeric(splitString[2], result)) {
+			var2 = result[0];
+		}
+		return var1 / var2;
 	}
 
 	public static double multiply(String[] splitString) {
-		return 0;
+		double var1 = 0;
+		double var2 = 0;
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		if (isNumeric(splitString[2], result)) {
+			var2 = result[0];
+		}
+		return var1 * var2;
 	}
 
 	public static double subtract(String[] splitString) {
-		return 0;
+		double var1 = 0;
+		double var2 = 0;
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		if (isNumeric(splitString[2], result)) {
+			var2 = result[0];
+		}
+		return var1 - var2;
 	}
 
 	public static double addNumber(String[] splitString) {
-		return 0;
+		double var1 = 0;
+		double var2 = 0;
+		double[] result = new double[1];
+		if (isNumeric(splitString[1], result)) {
+			var1 = result[0];
+		}
+		if (isNumeric(splitString[2], result)) {
+			var2 = result[0];
+		}
+		return var1 + var2;
 	}
 
 	public static boolean isNumeric(String string, double[] result) {
+		try {
+			double test = Double.parseDouble(string);
+			result[0] = test;
+		} catch (Exception e) {
+			return false;
+		}
 		return true;
 	}
+
+	public static boolean isWhiteSpace(String[] splitString) {
+		
+		for(int i=0; i<splitString.length;i++) {
+			String c = splitString[i];
+			if(c.equals(" ")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 
 }
--- CS200/P10/src/DefinitelyNotACalculatorTest.java
+++ CS200/P10/src/DefinitelyNotACalculatorTest.java
@@ -5,127 +5,126 @@ import org.junit.jupiter.api.Test;
 class DefinitelyNotACalculatorTest {
 
 	@Test
-	void testIsNumeric() {
-		double[] result= new double [1];
+	public void testIsNumeric() {
+		double[] result = new double[1];
 		assertTrue(DefinitelyNotACalculator.isNumeric("1", result));
-		assertTrue(result[0]==1);
+		assertTrue(result[0] == 1);
 		assertTrue(DefinitelyNotACalculator.isNumeric("1.5", result));
-		assertTrue(result[0]==1.5);
+		assertTrue(result[0] == 1.5);
 		assertTrue(!DefinitelyNotACalculator.isNumeric("F", result));
 		assertTrue(!DefinitelyNotACalculator.isNumeric("", result));
 	}
 
 	@Test
-	void testAddNumberInt() {
-		String[] inputs = {"","1","2"};
-		assertTrue(DefinitelyNotACalculator.addNumber(inputs)==3);
+	public void testAddNumberInt() {
+		String[] inputs = { "", "1", "2" };
+		assertTrue(DefinitelyNotACalculator.addNumber(inputs) == 3);
 	}
 
 	@Test
-	void testAddNumberDbl() {
-		String[] inputs = {"","1.5","2.3"};
-		assertTrue(DefinitelyNotACalculator.addNumber(inputs)==3.8);
+	public void testAddNumberDbl() {
+		String[] inputs = { "", "1.5", "2.3" };
+		assertTrue(DefinitelyNotACalculator.addNumber(inputs) == 3.8);
 	}
 
 	@Test
-	void testAddNumberFail() {
-		String[] inputs = {"","F","2.3"};
-		assertTrue(DefinitelyNotACalculator.addNumber(inputs)==2.3);
-		inputs = new String[] {"","2.3","F"};
-		assertTrue(DefinitelyNotACalculator.addNumber(inputs)==2.3);
-		inputs = new String[] {"","F","F"};
-		assertTrue(DefinitelyNotACalculator.addNumber(inputs)==0);
+	public void testAddNumberFail() {
+		String[] inputs = { "", "F", "2.3" };
+		assertTrue(DefinitelyNotACalculator.addNumber(inputs) == 2.3);
+		inputs = new String[] { "", "2.3", "F" };
+		assertTrue(DefinitelyNotACalculator.addNumber(inputs) == 2.3);
+		inputs = new String[] { "", "F", "F" };
+		assertTrue(DefinitelyNotACalculator.addNumber(inputs) == 0);
 	}
 
 	@Test
-	void testIsTwoNumbers() {
-		String[] inputs = {"","1","2"};
+	public void testIsTwoNumbers() {
+		String[] inputs = { "", "1", "2" };
 		assertTrue(DefinitelyNotACalculator.isTwoNumbers(inputs));
 	}
 
 	@Test
-	void testIsValidArguments() {
-		String[] inputs = {DefinitelyNotACalculator.PLUS,"1","2"};
+	public void testIsValidArguments() {
+		String[] inputs = { DefinitelyNotACalculator.PLUS, "1", "2" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.SUBSTRACT,"1","2"};
+		inputs = new String[] { DefinitelyNotACalculator.SUBTRACT, "1", "2" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.MULTIPLY,"1","2"};
+		inputs = new String[] { DefinitelyNotACalculator.MULTIPLY, "1", "2" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.DIVIDE,"1","2"};
+		inputs = new String[] { DefinitelyNotACalculator.DIVIDE, "1", "2" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.MAX,"1","2"};
+		inputs = new String[] { DefinitelyNotACalculator.MAX, "1", "2" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.POW,"1","2"};
-		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
-		
-		inputs = new String[] {DefinitelyNotACalculator.ABS,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.POW, "1", "2" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
 
-		inputs = new String[] {DefinitelyNotACalculator.LOG,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.ABS, "1" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
 
-		inputs = new String[] {DefinitelyNotACalculator.SQRT,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.LOG, "1" };
+		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
+
+		inputs = new String[] { DefinitelyNotACalculator.SQRT, "1" };
 		assertTrue(DefinitelyNotACalculator.isValidArgument(inputs));
 	}
 
 	@Test
-	void testIsValidArgumentsFail() {
-		String[] inputs = {DefinitelyNotACalculator.PLUS,"2"};
+	public void testIsValidArgumentsFail() {
+		String[] inputs = { DefinitelyNotACalculator.PLUS, "2" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.SUBSTRACT,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.SUBTRACT, "1" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.MULTIPLY,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.MULTIPLY, "1" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.DIVIDE,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.DIVIDE, "1" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.MAX,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.MAX, "1" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[]{DefinitelyNotACalculator.POW,"1"};
+		inputs = new String[] { DefinitelyNotACalculator.POW, "1" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		
-		inputs = new String[] {DefinitelyNotACalculator.ABS,"1","2"};
+
+		inputs = new String[] { DefinitelyNotACalculator.ABS, "1", "2" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[] {DefinitelyNotACalculator.LOG,"1","2"};
+		inputs = new String[] { DefinitelyNotACalculator.LOG, "1", "2" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
-		inputs = new String[] {DefinitelyNotACalculator.SQRT,"1","2"};
+		inputs = new String[] { DefinitelyNotACalculator.SQRT, "1", "2" };
 		assertTrue(!DefinitelyNotACalculator.isValidArgument(inputs));
 	}
 
 	@Test
-	void testIsTwoNumbersFail() {
-		String[] inputs = {"","F","2.3"};
+	public void testIsTwoNumbersFail() {
+		String[] inputs = { "", "F", "2.3" };
 		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
-		inputs = new String[] {"","2.3","F"};
+		inputs = new String[] { "", "2.3", "F" };
 		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
-		inputs = new String[] {"","F","F"};
+		inputs = new String[] { "", "F", "F" };
 		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
-		inputs = new String[] {"","1","2","3"};
+		inputs = new String[] { "", "1", "2", "3" };
 		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
-		inputs = new String[] {"","1"};
-		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
-
-		inputs = new String[]{"Quit"};
-		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
-		inputs = new String[]{"exit"};
+		inputs = new String[] { "", "1" };
 		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
 
-		inputs = new String[]{"quit"};
+		inputs = new String[] { "Quit" };
+		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
+		inputs = new String[] { "exit" };
 		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
 
-		//inputs = new String[]{"quit","2","3"};
-		//assertTrue(!DefinitelyNotACalculator.isValidArguments(inputs));
-	
+		inputs = new String[] { "quit" };
+		assertTrue(!DefinitelyNotACalculator.isTwoNumbers(inputs));
+
+		// inputs = new String[]{"quit","2","3"};
+		// assertTrue(!DefinitelyNotACalculator.isValidArguments(inputs));
+
 	}
 
 	@Test
-	void testIsValidOpt() {
-		String input="+";
+	public void testIsValidOpt() {
+		String input = "+";
 		assertTrue(DefinitelyNotACalculator.isValidOperator(input));
 		assertTrue(DefinitelyNotACalculator.isValidOperator("-"));
 		assertTrue(DefinitelyNotACalculator.isValidOperator("*"));
 		assertTrue(DefinitelyNotACalculator.isValidOperator("/"));
 
-		
 		assertTrue(!DefinitelyNotACalculator.isValidOperator("quit"));
 		assertTrue(!DefinitelyNotACalculator.isValidOperator("Quit"));
 		assertTrue(!DefinitelyNotACalculator.isValidOperator("QUIT"));
@@ -133,27 +132,27 @@ class DefinitelyNotACalculatorTest {
 	}
 
 	@Test
-	void testRandArguments() {
-		String[] inputs = {"","F","2.3"};
+	public void testRandArguments() {
+		String[] inputs = { "", "F", "2.3" };
 		assertTrue(!DefinitelyNotACalculator.isValidRandArgument(inputs));
-		inputs = new String[] {"","2.3"};
+		inputs = new String[] { "", "2.3" };
 		assertTrue(DefinitelyNotACalculator.isValidRandArgument(inputs));
-		inputs = new String[] {"","2.3","5"};
+		inputs = new String[] { "", "2.3", "5" };
 		assertTrue(!DefinitelyNotACalculator.isValidRandArgument(inputs));
 	}
 
 	@Test
-	void testIsInt() {
+	public void testIsInt() {
 		assertTrue(DefinitelyNotACalculator.isInt(1));
 		assertTrue(DefinitelyNotACalculator.isInt(-1));
 		assertTrue(DefinitelyNotACalculator.isInt(0));
 		assertTrue(!DefinitelyNotACalculator.isInt(1.5));
 		assertTrue(!DefinitelyNotACalculator.isInt(-1.5));
 	}
-	
-	@Test 
-	void testIsEndlessArg() {
-		String[] splitString = {"", "2","3","5","23","5"};
+
+	@Test
+	public void testIsEndlessArg() {
+		String[] splitString = { "", "2", "3", "5", "23", "5" };
 		assertTrue(DefinitelyNotACalculator.isValidEndlessArgument(splitString));
 	}
 }

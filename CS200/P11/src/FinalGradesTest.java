d69bbbddec7969921972fe3d3cc649f7f580abb8
alvickyfun <alvickyfun@gmail.com>
2019-08-02 21:11:59 -0500

    add junit

---
+++ CS200/P11/src/FinalGradesTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FinalGradesTest {
+
+	@Test
+	public void testComputeRawFinalGrades() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeLetterGrades() {
+		fail("Not yet implemented");
+	}
+
+}

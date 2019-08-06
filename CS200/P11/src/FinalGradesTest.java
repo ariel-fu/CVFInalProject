a2231b10a12b832f62bcb2829b0aea334a0491ee
alvickyfun <Ariel@192.168.1.116>
2019-08-05 19:41:43 -0500

    add test cases

--- CS200/P11/src/FinalGradesTest.java
+++ CS200/P11/src/FinalGradesTest.java
@@ -1,17 +1,33 @@
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
+import java.util.Arrays;
 
 public class FinalGradesTest {
 
 	@Test
 	public void testComputeRawFinalGrades() {
-		fail("Not yet implemented");
+		double[][] grades = { { 5, 6, 7 }, { 0, 0, 0 }, { 10, 12, 14 } };
+		int[] maxPoints = { 10, 12, 14 };
+		ArrayList<Double> b = new ArrayList<Double>();
+		b.add(0.5);
+		b.add(0.0);
+		b.add(1.0);
+		ArrayList<Double> a = FinalGrades.computeRawFinalGrades(grades, maxPoints);
+		assertTrue(a.equals(b));
 	}
 
 	@Test
 	public void testComputeLetterGrades() {
-		fail("Not yet implemented");
+		ArrayList<Double> rawGrades = new ArrayList<Double>();
+		rawGrades.add(0.5);
+		rawGrades.add(0.0);
+		rawGrades.add(1.0);
+		String[] letters = {"A", "B", "C", "F"};
+		int[] breakdown = {1,1,1};
+		String[] return1 = {"B", "C", "A"};
+		String[] result = FinalGrades.computeLetterGrades(rawGrades, letters, breakdown);
+		
 	}
 
 }

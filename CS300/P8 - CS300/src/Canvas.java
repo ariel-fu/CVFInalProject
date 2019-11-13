752fe08916d1a21864f3085a9dd680196e4c86af
Ariel <Ariel@192.168.1.120>
2019-11-12 22:46:04 -0600

    Changes to Canvas

--- CS300/P8 - CS300/src/Canvas.java
+++ CS300/P8 - CS300/src/Canvas.java
@@ -29,195 +29,217 @@ import java.util.EmptyStackException;
  *
  */
 public class Canvas {
-	private final int width; // width of the canvas
-	private final int height; // height of the canvas
-	private char[][] drawingArray; // 2D character array to store the drawing
-	private final DrawingStack undoStack; // store previous changes for undo.
-	private final DrawingStack redoStack; // store undone changes for redo
+  private final int width; // width of the canvas
+  private final int height; // height of the canvas
+  private char[][] drawingArray; // 2D character array to store the drawing
+  private final DrawingStack undoStack; // store previous changes for undo.
+  private final DrawingStack redoStack; // store undone changes for redo
 
-	/**
-	 * Constructor creates a new canvas which is initially blank. (Uses spaces for
-	 * empty space.)
-	 * 
-	 * @param width  - width of the canvas
-	 * @param height - height of the canvas
-	 * @throws IllegalArgumentException - if the height is 0 or less OR if the width
-	 *                                  is 0 or less
-	 */
-	public Canvas(int width, int height) {
-		// if the height or the width is less than or equal to , throw an
-		// IllegalArgumentException.
-		if (width <= 0 || height <= 0) {
-			throw new IllegalArgumentException(
-					"Out of bounds for width or height: " + width + " width value " + height + " height value.");
-		}
-		drawingArray = new char[height][width];
-		for (int row = 0; row < height; row++) {
-			for (int column = 0; column < width; column++) {
-				drawingArray[row][column] = ' ';
-			}
-		}
-		// set the width and the height accordingly.
-		this.width = width;
-		this.height = height;
-		// then create two new DrawingStack.
-		undoStack = new DrawingStack();
-		redoStack = new DrawingStack();
-	}
+  /**
+   * Constructor creates a new canvas which is initially blank. (Uses spaces for
+   * empty space.)
+   * 
+   * @param width  - width of the canvas
+   * @param height - height of the canvas
+   * @throws IllegalArgumentException - if the height is 0 or less OR if the width
+   *                                  is 0 or less
+   */
+  public Canvas(int width, int height) {
+    // if the height or the width is less than or equal to , throw an
+    // IllegalArgumentException.
+    if (width <= 0 || height <= 0) {
+      throw new IllegalArgumentException("Out of bounds for width or height: " + width
+          + " width value " + height + " height value.");
+    }
+    drawingArray = new char[height][width];
+    for (int row = 0; row < height; row++) {
+      for (int column = 0; column < width; column++) {
+        drawingArray[row][column] = ' ';
+      }
+    }
+    // set the width and the height accordingly.
+    this.width = width;
+    this.height = height;
+    // then create two new DrawingStack.
+    undoStack = new DrawingStack();
+    redoStack = new DrawingStack();
+  }
 
-	/**
-	 * Draws a character at the given position drawingArray[row][col]. If there is
-	 * already a char, overwrite that char
-	 * 
-	 * @param row - row of where to draw the character
-	 * @param col - col of where to draw the character
-	 * @param c   - character to be placed at [row][col]
-	 * @throws IllegalArgumentException - if the row and the col are out of bounds /
-	 *                                  off the canvas
-	 */
-	public void draw(int row, int col, char c) {
-		// determine if the row & col given are in-bounds, throw
-		// IllegalArgumentException if the row and the col are out of bounds.
-		if (row >= width || col >= height || row < 0 || col < 0) {
-			throw new IllegalArgumentException("The row and the column given are out of bounds.");
-		}
-		// determine the previous character at [row][col]
-		DrawingChange currChange = new DrawingChange(row, col, drawingArray[row][col], c);
-		// create the new drawing change, and set the character at [row][col] to the new
-		// character - c.
+  /**
+   * Draws a character at the given position drawingArray[row][col]. If there is
+   * already a char, overwrite that char
+   * 
+   * @param row - row of where to draw the character
+   * @param col - col of where to draw the character
+   * @param c   - character to be placed at [row][col]
+   * @throws IllegalArgumentException - if the row and the col are out of bounds /
+   *                                  off the canvas
+   */
+  public void draw(int row, int col, char c) {
+    // determine if the row & col given are in-bounds, throw
+    // IllegalArgumentException if the row and the col are out of bounds.
+    if (!validRowCol(row, col)) {
+      throw new IllegalArgumentException("The row and the column given are out of bounds.");
+    }
+    // determine the previous character at [row][col]
+    DrawingChange currChange = new DrawingChange(row, col, drawingArray[row][col], c);
+    // create the new drawing change, and set the character at [row][col] to the new
+    // character - c.
+    drawingArray[row][col] = c;
+    // push the recent drawing change to the undo stack
+    undoStack.push(currChange);
+    // After making a new change, clear the redoStack.
+    while (!redoStack.isEmpty()) {
+      redoStack.pop();
+    }
+  }
 
-		drawingArray[row][col] = c;
-		// push the recent drawing change to the undo stack
-		undoStack.push(currChange);
-		// After making a new change, clear the redoStack.
-		while (!redoStack.isEmpty()) {
-			redoStack.pop();
-		}
-	}
+  /**
+   * Checks if the row and the col are within bounds.
+   * 
+   * @param row - row
+   * @param col - col
+   * @throws IllegalArgumentException if the row or the col input are out of
+   *                                  bounds
+   * @return true if the row and the col given are within bounds.
+   */
+  private boolean validRowCol(int row, int col) {
+    if (row >= width || col >= height || row < 0 || col < 0) {
+      return false;
+    }
 
-	/**
-	 * Undo the most recent drawing change, then updates the contents of the
-	 * drawingArray appropriately.
-	 * 
-	 * @return true if the undone DrawingChange is popped off the undoStack and if
-	 *         the DrawingChange is pushed onto the redoStack.
-	 */
-	public boolean undo() {
-		// get the most recent drawing change
-		DrawingChange recentDrawingChange = undoStack.pop();
+    return true;
+  }
 
-		// push the undo DrawingChange onto the redoStack.
-		redoStack.push(recentDrawingChange);
+  /**
+   * Undo the most recent drawing change, then updates the contents of the
+   * drawingArray appropriately.
+   * 
+   * @return true if the undone DrawingChange is popped off the undoStack and if
+   *         the DrawingChange is pushed onto the redoStack.
+   */
+  public boolean undo() {
+    DrawingChange recentDrawingChange;
+    try {
+      // get the most recent drawing change
+      recentDrawingChange = undoStack.pop();
+    } catch (EmptyStackException e) {
+      return false;
+    }
+    // push the undo DrawingChange onto the redoStack.
+    redoStack.push(recentDrawingChange);
 
-		// set the new character at [row][col] to the previous character
-		drawingArray[recentDrawingChange.row][recentDrawingChange.col] = recentDrawingChange.prevChar;
+    // set the new character at [row][col] to the previous character
+    drawingArray[recentDrawingChange.row][recentDrawingChange.col] = recentDrawingChange.prevChar;
+    // return if the undo method worked as expected.
+    return evaluate(undoStack, recentDrawingChange);
+  }
 
-		// check if the undoStack and the redoStack have been changed appropriately.
-		try {
-			// top of the stack should not be equal to the most recent change.
-			if (undoStack.peek() == recentDrawingChange) {
-				return false;
-			}
-		} catch (EmptyStackException e) {
-		} // should throw an EmptyStackException if the stack is empty after the undo.
-		catch (Exception e) {
-			return false; // if any other EmptyStackException is thrown, return false.
-		}
-		if (redoStack.peek() != recentDrawingChange) {
-			return false;
-		}
-		if (drawingArray[recentDrawingChange.row][recentDrawingChange.col] != recentDrawingChange.prevChar) {
-			return false;
-		}
+  /**
+   * Evaluates if the undo or redo method popped and pushed onto the Stack the
+   * correct DrawingChanges.
+   * 
+   * @param stack         - undo or redo stack
+   * @param drawingChange - the most recent DrawingChange done to the Canvas
+   * @return
+   */
+  private boolean evaluate(DrawingStack stack, DrawingChange drawingChange) {
 
-		return true;
-	}
+    // check if the undoStack and the redoStack have been changed appropriately.
+    try {
+      // top of the stack should not be equal to the most recent change.
+      if (stack.peek() == drawingChange) {
+        return false;
+      }
+    } catch (EmptyStackException e) {
+    } // should throw an EmptyStackException if the stack is empty after the undo.
+    catch (Exception e) {
+      return false; // if any other EmptyStackException is thrown, return false.
+    }
+    if (redoStack.peek() != drawingChange) {
+      return false;
+    }
+    if (drawingArray[drawingChange.row][drawingChange.col] != drawingChange.prevChar) {
+      return false;
+    }
+    // if all test cases pass, return true.
+    return true;
 
-	/**
-	 * Redo the most recent undone drawing change, then updates the drawingArray
-	 * accordingly.
-	 * 
-	 * @return true if the redone DrawingChange is popped off the redoStack and if
-	 *         the Drawing Change is pushed (back) onto the redoStack.
-	 */
-	public boolean redo() {
-		// get the most recent undo change from the redoStack
-		DrawingChange mostRecentUndo = redoStack.pop();
+  }
 
-		// push the most recent undo change (back) onto the undoStack.
-		undoStack.push(mostRecentUndo);
+  /**
+   * Redo the most recent undone drawing change, then updates the drawingArray
+   * accordingly.
+   * 
+   * @return true if the redone DrawingChange is popped off the redoStack and if
+   *         the Drawing Change is pushed (back) onto the redoStack.
+   */
+  public boolean redo() {
+    DrawingChange recentDrawingChange;
+    try {
+      // get the most recent undo change from the redoStack
+      recentDrawingChange = redoStack.pop();
+    } catch (EmptyStackException e) {
+      return false;
+    }
+    // push the most recent undo change (back) onto the undoStack.
+    undoStack.push(recentDrawingChange);
 
-		// update the drawingArray to the new character given at the draw method (?)
-		drawingArray[mostRecentUndo.row][mostRecentUndo.col] = mostRecentUndo.newChar;
+    // update the drawingArray to the new character given at the draw method (?)
+    drawingArray[recentDrawingChange.row][recentDrawingChange.col] = recentDrawingChange.newChar;
 
-		try {
-			// checks if the changes were made, the top of the stack should not be the one
-			// that was just popped off.
-			if (redoStack.peek() == mostRecentUndo) {
-				return false;
-			}
-		} catch (EmptyStackException e) { // this peek method should throw a EmptyStackException if the
-											// stack is
-			// empty after the redo.
-		} catch (Exception e) {
-			return false;
-		}
-		if (undoStack.peek() != mostRecentUndo) {
-			return false;
-		}
-		if (drawingArray[mostRecentUndo.row][mostRecentUndo.col] != mostRecentUndo.newChar) {
-			return false;
-		}
-		// otherwise, it was a successful redo!
-		return true;
-	}
+    // otherwise, it was a successful redo!
+    return evaluate(redoStack, recentDrawingChange);
+  }
 
-	/**
-	 * Prints out the history of redos and undos
-	 */
-	public void printHistory() {
+  /**
+   * Prints out the history of redos and undos
+   */
+  public void printHistory() {
 
-		int i = undoStack.size();
-		for (DrawingChange changes : undoStack) {
-			System.out.println(
-					"#" + i + ". draw '" + changes.newChar + "' on (" + changes.row + ", " + changes.col + ")");
-			i--;
+    int i = undoStack.size();
+    for (DrawingChange changes : undoStack) {
+      System.out.println("#" + i + ". draw '" + changes.newChar + "' on (" + changes.row + ", "
+          + changes.col + ")");
+      i--;
 
-		}
-	}
+    }
+  }
 
-	/**
-	 * Prints out the String version of the Canvas
-	 */
-	public void printDrawing() {
-		System.out.println(this.toString());
-	}
+  /**
+   * Prints out the String version of the Canvas
+   */
+  public void printDrawing() {
+    System.out.println(this.toString());
+  }
 
-	/**
-	 * Returns a printable string version of the Canvas
-	 * 
-	 * @return String version of the Canvas
-	 */
-	public String toString() {
-		String stringVersion = "";
-		for (int row = 0; row < drawingArray.length; row++) {
-			for (int column = 0; column < drawingArray[row].length; column++) {
+  /**
+   * Returns a printable string version of the Canvas
+   * 
+   * @return String version of the Canvas
+   */
+  @Override
+  public String toString() {
+    String stringVersion = "";
+    for (int row = 0; row < drawingArray.length; row++) {
+      for (int column = 0; column < drawingArray[row].length; column++) {
 
-				stringVersion += drawingArray[row][column];
+        stringVersion += drawingArray[row][column];
 
-			}
-			// add a new line
-			stringVersion += System.lineSeparator();
-		}
-		return stringVersion;
-	}
-	
-	/**
-	 * Returns the char[][] that represents the Canvas
-	 * @return drawingArray - representation of the Canvas
-	 */
-	public char[][] getCanvas() {
-		return drawingArray;
-	}
+      }
+      // add a new line
+      stringVersion += System.lineSeparator();
+    }
+    return stringVersion;
+  }
+
+  /**
+   * Returns the char[][] that represents the Canvas
+   * 
+   * @return drawingArray - representation of the Canvas
+   */
+  public char[][] getCanvas() {
+    return drawingArray;
+  }
 
 }

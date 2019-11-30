872a724c27bb5a059793aefa9897b6f48443ecce
alvickyfun <Ariel@192.168.1.179>
2019-11-30 13:58:21 -0600

    process scheduler

--- CS300/P10 - CS300/src/ProcessScheduler.java
+++ CS300/P10 - CS300/src/ProcessScheduler.java
@@ -29,7 +29,8 @@ import java.util.Scanner;
 public class ProcessScheduler {
   private int currentTime; // stores the current time after the last run
   private int numProcessesRun; // stores the number of processes run so far
-  private WaitingProcessQueue queue; // this processing unit’s queue
+  private WaitingProcessQueue queue; // this processing unit's queue
+  
 
   public ProcessScheduler() {
     queue = new WaitingProcessQueue();

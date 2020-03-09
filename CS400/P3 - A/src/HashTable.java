04a1a04a7ce2920e816b25051489cec8c465914d
alvickyfun <al.vicky.fun@gmail.com>
2020-03-09 08:46:29 -0500

    HashTable 3-9-2020

--- CS400/P3 - A/src/HashTable.java
+++ CS400/P3 - A/src/HashTable.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 // TODO: comment and complete your HashTableADT implementation
 // DO ADD UNIMPLEMENTED PUBLIC METHODS FROM HashTableADT and DataStructureADT TO YOUR CLASS
 // DO IMPLEMENT THE PUBLIC CONSTRUCTORS STARTED
@@ -10,9 +7,10 @@ import java.util.LinkedList;
 //
 // TODO: describe the collision resolution scheme you have chosen
 // identify your scheme as open addressing or bucket
-// I will use chaining to solve collisions.
+// I will use open addressing (double hashing) to solve collisions.
 //
 // TODO: explain your hashing algorithm here 
+// I am using .hashCode() to get the hash code for an element
 // NOTE: you are not required to design your own algorithm for hashing,
 //       since you do not know the type for K,
 //       you must use the hashCode provided by the <K key> object
@@ -20,6 +18,7 @@ import java.util.LinkedList;
 //
 public class HashTable<K extends Comparable<K>, V>
     implements HashTableADT<K, V> {
+
   private class Node {
     private K key;
     private V value;
@@ -36,12 +35,15 @@ public class HashTable<K extends Comparable<K>, V>
     private V getValue() {
       return value;
     }
+
+    private int hash() {
+      return key.hashCode();
+    }
   }
 
-  // TODO: ADD and comment DATA FIELD MEMBERS needed for your implementation
   private int tableSize; // curr capacity
   private double loadFactorThreshold; // threshold
-  private ArrayList<Node> hashTable;
+  private Object[] hashTable;
   private int currentNumberOfElements;
 
   /**
@@ -49,11 +51,11 @@ public class HashTable<K extends Comparable<K>, V>
    * load factor threshold to 3/4 full.
    */
   public HashTable() {
-    // set to default numbers, 103 and 3/4 full.
-    tableSize = 103;
+    // set to default numbers, 123 and the load factor threshold is 3/4 full.
+    tableSize = 123;
     loadFactorThreshold = 0.75;
     currentNumberOfElements = 0;
-    hashTable = new ArrayList<Node>();
+    hashTable = new Object[tableSize];
 
   }
 
@@ -69,87 +71,133 @@ public class HashTable<K extends Comparable<K>, V>
    *                            rehashing.
    */
   public HashTable(int initialCapacity, double loadFactorThreshold) {
+    // TODO: ask if need to do something if the IC and the LFT are negative
     this.tableSize = initialCapacity;
     this.loadFactorThreshold = loadFactorThreshold;
     this.currentNumberOfElements = 0;
+    hashTable = new Object[this.tableSize];
   }
 
+  /**
+   * Inserts a key-value pair into the hash table, will replace if the
+   * 
+   * @param key   - key associated with the pair
+   * @param value - value
+   * @throws IllegalNullKeyException -
+   */
   @Override
   public void insert(K key, V value) throws IllegalNullKeyException {
     if (key == null) {
       throw new IllegalNullKeyException("Key is null.");
     }
     int hashIndex = key.hashCode() % tableSize;
+    // check if need for rehashing and resizing
+    double loadFactor = currentNumberOfElements / (double) tableSize;
+    if (loadFactor >= loadFactorThreshold) {
+      // resize and rehash
+      this.resizeAndRehash();
+    }
 
+    Node nodeAtHashIndex = (Node) hashTable[hashIndex];
     // check if the hashIndex is null
-    if (hashTable.get(hashIndex) == null) {
+    if (hashTable[hashIndex] == null) {
       // insert
-      hashTable.set(hashIndex, new Node(key, value));
-
-      // check if need for rehashing and resizing
-      double loadFactor = currentNumberOfElements / tableSize;
-      if (loadFactor >= loadFactorThreshold) {
-        // resize and rehash
-
-      } else {
-        return;
-      }
+      hashTable[hashIndex] = new Node(key, value);
+      currentNumberOfElements++;
+      return;
+    } else if (nodeAtHashIndex.getKey().compareTo(key) == 0) {
+      // replace that key-value pair with a new key-value pair
+      hashTable[hashIndex] = new Node(key, value);
+      // increment the number of key-value pairs in the hash table
+      currentNumberOfElements++;
+      return;
     } else {
       // do double hashing for collision resolving
-   // start off because the hashIndex is not null
-      int currentIndex = hashIndex + 1; 
-      for (int i = 2; i < tableSize; i++) {
+      // start off because the hashIndex is not null
+      int currentIndex = hashIndex;
+      for (int i = 1; i < tableSize; i++) {
         // if currIndex == null, insert
-        if (hashTable.get(currentIndex) == null) {
-          // insert
-          hashTable.set(hashIndex, new Node(key, value));
+        if (hashTable[currentIndex] == null) {
+          // insert if the curr index is null
+          hashTable[currentIndex] = new Node(key, value);
 
-          // check if need for rehashing and resizing
-          
-
-        } else if (hashTable.get(currentIndex).key.compareTo(key) == 0) {
-          // insert
-          hashTable.set(hashIndex, new Node(key, value));
-
-          // check if need for rehashing and resizing
-          double loadFactor = currentNumberOfElements / tableSize;
-
-        } else {
-          // resize and rehash
+          // increment the number of key-value pairs in the hash table
+          currentNumberOfElements++;
+          return;
+          // check if it is on a key that is the same (replace)
+        } else if (nodeAtHashIndex.getKey().compareTo(key) == 0) {
+          // replace that key-value pair with a new key-value pair
+          hashTable[currentIndex] = new Node(key, value);
+          // increment the number of key-value pairs in the hash table
+          currentNumberOfElements++;
+          return;
         }
-        currentIndex += (i*i);
-      } // else resize and rehash
-      // load factor currently
-      double loadFactor = currentNumberOfElements / tableSize;
-      
-      if(loadFactor >= loadFactorThreshold) {
-        // resize and rehash
+        currentIndex = (currentIndex + (i * i)) % tableSize;
       }
-
     }
-  }
 
-  private void resize() {
-    ArrayList<Node> temp = hashTable;
-    
-    hashTable = new ArrayList<Node>();
-    
-    for(int i =0; i< hashTable.size()*2; i++) {
-      
-    }
-  }
-  @Override
-  public boolean remove(K key) throws IllegalNullKeyException {
-    // TODO Auto-generated method stub
-    return false;
+    currentNumberOfElements++;
   }
 
   /**
+   * Resizes the hash table, rehashes all elements and adds them to the new hash
+   * table
+   */
+  private void resizeAndRehash() {
+    // temp var to hold the current 3/4 full hash table
+    Object[] temp = hashTable;
+
+    // new hash table that has double size of the old hash table
+    hashTable = new Object[tableSize * 2 + 1];
+
+    // set the table size to double the old table size + 1
+    this.tableSize = hashTable.length;
+    for (int i = 0; i < temp.length; i++) {
+      // if the current index is an actual value, add to new hash table
+      if (temp[i] != null) {
+        Node currNode = (Node) temp[i];
+        int hashIndex = currNode.getKey().hashCode() % tableSize;
+        hashTable[hashIndex] = currNode;
+
+      }
+    }
+  }
+
+  /**
+   * Removes the Node given the key
    * 
-   * @param key
-   * @throws IllegalNullKeyException
-   * @throws KeyNotFoundException
-   * @return
+   * @param key - key associated with the node to remove
+   * @throws IllegalNullKeyException - null key
+   * @return true if the key was in the hash table and was properly removed
+   */
+  @Override
+  public boolean remove(K key) throws IllegalNullKeyException {
+    if (key == null) {
+      throw new IllegalNullKeyException("Null key, cannot remove");
+    }
+    Node removeNode = this.getNode(key);
+    // if getNode() returned null, the key is not in the hash table
+    if (removeNode == null) {
+      return false;
+    }
+    // else, the key is in the hash table, so get the hash index
+    int keyHashIndex = removeNode.getKey().hashCode() % tableSize;
+
+    // set the key at the hash index to a new key that marks it removed - null
+    hashTable[keyHashIndex] = null;
+    // decrease the number of elements in the table
+    this.currentNumberOfElements--;
+
+    return true; // removed :)
+  }
+
+  /**
+   * Gets the value associated with the key
+   * 
+   * @param key - key of the node with the value
+   * @throws IllegalNullKeyException - if the key is null
+   * @throws KeyNotFoundException    - if the key is not in the hash table
+   * @return the value associated with the key
    */
   @Override
   public V get(K key) throws IllegalNullKeyException, KeyNotFoundException {
@@ -159,35 +207,54 @@ public class HashTable<K extends Comparable<K>, V>
 
     Node keyNode = getNode(key);
     if (keyNode == null) {
-      throw new KeyNotFoundException(
-          "Key is not in the hash table, cannot get it.");
+      throw new KeyNotFoundException("Tried to get this key: " + key);
     } else {
       return keyNode.getValue();
     }
   }
 
+  /**
+   * Gets the number of elements in the hash table
+   * 
+   * @return the number of keys / elements
+   */
   @Override
   public int numKeys() {
-    // TODO Auto-generated method stub
-    return 0;
+    return currentNumberOfElements;
   }
 
+  /**
+   * Gets the load factor threshold, how full the table can get before resizing
+   * and rehashing is needed.
+   * 
+   * @return the load factor threshold
+   */
   @Override
   public double getLoadFactorThreshold() {
-    // TODO Auto-generated method stub
-    return 0;
+    return loadFactorThreshold;
   }
 
+  /**
+   * Returns how full the table is, also called the load factor
+   * 
+   * @return number of elements divided by the table size
+   */
   @Override
   public double getLoadFactor() {
-    // may need fixing TODO
-    return this.loadFactorThreshold * tableSize;
+
+    // (how full the table is)
+    return currentNumberOfElements / tableSize;
   }
 
+  /**
+   * Returns the table size of the hash table
+   * 
+   * @return tableSize of the hash table
+   */
   @Override
   public int getCapacity() {
-    // TODO Auto-generated method stub
-    return 0;
+
+    return tableSize;
   }
 
   /**
@@ -197,7 +264,6 @@ public class HashTable<K extends Comparable<K>, V>
    */
   @Override
   public int getCollisionResolution() {
-    // TODO Auto-generated method stub
     return 3;
   }
 
@@ -208,17 +274,23 @@ public class HashTable<K extends Comparable<K>, V>
    * @return null if the key is not in the hash table or the node
    */
   private Node getNode(K key) {
-    int index = key.hashCode() % tableSize;
-    for (Node node : hashTable) {
-      if (node == null) {
 
-      } else if (node.key.compareTo(key) == 0) {
-        return node; // key is in hash table
+    int hashIndex = Math.abs(key.hashCode() % tableSize);
+    Node keyNode = (Node) hashTable[hashIndex];
+    if (keyNode != null) {
+      if (keyNode.getKey().compareTo(key) != 0) {
+        for (int i = 1; i < tableSize; i++) {
+          Node currNode = (Node) hashTable[i];
+          if (currNode != null) {
+            if (currNode.getKey().compareTo(key) == 0) {
+              return currNode; // found key!
+            }
+          }
+        }
+        return null; // key is not in the hash table
       }
     }
-    return null; // key is not in hash table
-
+    return keyNode;
   }
-  // TODO: implement all unimplemented methods so that the class can compile
 
 }
--- CS400/P3 - A/src/HashTableTest.java
+++ CS400/P3 - A/src/HashTableTest.java
@@ -1,61 +1,251 @@
 // TODO: add imports as needed
 
 // org.junit.Assert.*; 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/** TODO: add class header comments here */
+public class HashTableTest {
 
+  // TODO: add other fields that will be used by multiple tests
+  private HashTable<Integer, Integer> ht;
 
-/** TODO: add class header comments here*/
-public class HashTableTest{
+  // TODO: add code that runs before each test method
+  @Before
+  public void setUp() throws Exception {
+    ht = new HashTable<Integer, Integer>(11, 0.5);
+  }
 
-    // TODO: add other fields that will be used by multiple tests
-    
-    // TODO: add code that runs before each test method
-    @Before
-    public void setUp() throws Exception {
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   * Tests that a HashTable returns an integer code indicating which collision
+   * resolution strategy is used. REFER TO HashTableADT for valid collision
+   * scheme codes.
+   */
+  @Test
+  public void test000_collision_scheme() {
+    HashTableADT htIntegerKey = new HashTable<Integer, String>();
+    int scheme = htIntegerKey.getCollisionResolution();
+    if (scheme < 1 || scheme > 9)
+      fail("collision resolution must be indicated with 1-9");
+  }
+
+  /**
+   * IMPLEMENTED AS EXAMPLE FOR YOU Tests that insert(null,null) throws
+   * IllegalNullKeyException
+   */
+  @Test
+  public void test001_IllegalNullKey() {
+    try {
+      HashTableADT htIntegerKey = new HashTable<Integer, String>();
+      htIntegerKey.insert(null, null);
+      fail("should not be able to insert null key");
+    } catch (IllegalNullKeyException e) {
+      /* expected */ } catch (Exception e) {
+      fail("insert null key should not throw exception "
+          + e.getClass().getName());
+    }
+  }
+
+  /**
+   * Tests all values are set to the "default"
+   */
+  @Test
+  public void test_no_arg_constructor() {
+    HashTable no_arg = new HashTable<Integer, Integer>();
+    assertTrue(no_arg.getCapacity() == 123);
+    assertTrue(no_arg.getLoadFactorThreshold() == 0.75);
+  }
+
+  /**
+   * Tests all the values are set to the ones inputted.
+   */
+  @Test
+  public void test_constructor() {
+    assertTrue(ht.getCapacity() == 11);
+    assertTrue(ht.getLoadFactorThreshold() == 0.5);
+  }
+
+  /**
+   * Tests a simple insert
+   */
+  @Test
+  public void test_simple_insert() {
+    try {
+      ht.insert(20, 20);
+      ht.insert(30, 30);
+      ht.insert(40, 40);
+      assertTrue(ht.numKeys() == 3);
+    } catch (IllegalNullKeyException e) {
+      fail("No exceptions should be thrown from simple insert");
+    }
+  }
+
+  /**
+   * Tests a rehashing and resizing
+   */
+  @Test
+  public void test_need_rehash_and_resize() {
+    try {
+      for (int i = 0; i < 8; i++) {
+        ht.insert(i, i);
+      }
+
+      assertTrue(ht.getCapacity() == 23);
+      assertTrue(ht.numKeys() == 8);
+    } catch (IllegalNullKeyException e) {
+      fail("No exceptions should be thrown from rehashing and resizing");
+    }
+  }
+
+  /**
+   * Tests replacing a value
+   */
+  @Test
+  public void test_replace() {
+    try {
+      ht.insert(20, 10);
+      ht.insert(20, 50);
+      assertTrue(ht.get(20) == 50);
+    } catch (IllegalNullKeyException e) {
+      fail("No exceptions should've been thrown from replacing");
+    } catch (KeyNotFoundException e) {
+      fail("No exceptions should've been thrown from replacing");
+    }
+  }
+
+  /**
+   * Tests a simple remove
+   */
+  @Test
+  public void test_remove_simple() {
+    try {
+      ht.insert(1, 1);
+      ht.insert(2, 2);
+      ht.insert(9, 9);
+      ht.insert(50, 50);
+      ht.insert(12, 12);
+      assertTrue(ht.remove(1));
+      assertTrue(ht.numKeys() == 4);
+    } catch (IllegalNullKeyException e) {
+      fail("No exceptions should've been thrown from a simple remove");
+    }
+  }
+
+  /**
+   * Tests inserting, removing then getting that value
+   */
+  @Test
+  public void test_insert_remove_get() {
+    try {
+      ht.insert(20, 20);
+      assertTrue(ht.remove(20));
+      ht.get(20);
+    } catch (IllegalNullKeyException e) {
+      fail("No exceptions should be thrown from a insert/remove/get");
+    } catch (KeyNotFoundException e) {
+      assertTrue(ht.numKeys() == 0);
+    } catch (Exception e) {
+      fail("No exceptions should be thrown from a insert/remove/get");
+    }
+  }
+
+  /**
+   * Remove all keys
+   */
+  @Test
+  public void test_remove_all() {
+    try {
+
+      // insert 10 values
+      for (int i = 0; i < 10; i++) {
+        ht.insert(i, i);
+      }
+      // remove all the values
+      for (int i = 0; i < 10; i++) {
+        assertTrue(ht.remove(i));
+      }
+
+      ht.get(2);
+    } catch (KeyNotFoundException e) {
+      assertTrue(ht.numKeys() == 0);
+    } catch (Exception e) {
+      fail("No exceptions should be thrown from remove all");
+    }
+  }
+  // TODO add your own tests of your implementation
+
+  /**
+   * Tries to get a key that is not in hash table
+   */
+  @Test
+  public void test_get_nothing_there() {
+    try {
+      ht.insert(5, 5);
+      ht.insert(2, 2);
+      ht.insert(9, 9);
+      ht.insert(1, 1);
+      ht.insert(0, 0);
+      ht.get(4);
+    } catch (KeyNotFoundException e) {
+
+    } catch (IllegalNullKeyException e) {
 
     }
+  }
 
-    // TODO: add code that runs after each test method
-    @After
-    public void tearDown() throws Exception {
+  @Test
+  public void test_get_null() {
+    try {
+      ht.get(null);
+    } catch (IllegalNullKeyException e) {
 
+    } catch (Exception e) {
+      fail("No exceptions should be thrown from get");
     }
-    
-    /** 
-     * Tests that a HashTable returns an integer code
-     * indicating which collision resolution strategy 
-     * is used.
-     * REFER TO HashTableADT for valid collision scheme codes.
-     */
-    @Test
-    public void test000_collision_scheme() {
-        HashTableADT htIntegerKey = new HashTable<Integer,String>();
-        int scheme = htIntegerKey.getCollisionResolution();
-        if (scheme < 1 || scheme > 9) 
-            fail("collision resolution must be indicated with 1-9");
+  }
+
+  /**
+   * Tests #18 on Gradescope
+   */
+  @Test
+  public void test_get_247001() {
+    try {
+      ht.insert(247001, 01);
+      ht.insert(00, 00);
+      ht.insert(80, 80);
+
+      ht.insert(30, 30);
+      ht.insert(27, 27);
+      ht.insert(23, 23);
+      ht.insert(49, 49);
+
+      ht.remove(247001);
+
+      ht.insert(50, 50);
+      ht.insert(12, 12);
+      ht.insert(247001, 01);
+
+      assertTrue(ht.numKeys() == 9);
+      ht.insert(15, 15);
+      ht.insert(93, 93);
+      assertTrue(ht.numKeys() == 11);
+      Integer one = 01;
+      System.out.println(one.hashCode());
+      System.out.println(ht.get(247001));
+
+      assertTrue(ht.get(247001) == 01);
+    } catch (KeyNotFoundException e) {
+      fail("No exceptions" + e.getMessage());
+    } catch (Exception e) {
+      fail("No excpetions");
     }
-        
-    /** IMPLEMENTED AS EXAMPLE FOR YOU
-     * Tests that insert(null,null) throws IllegalNullKeyException
-     */
-    @Test
-    public void test001_IllegalNullKey() {
-        try {
-            HashTableADT htIntegerKey = new HashTable<Integer,String>();
-            htIntegerKey.insert(null, null);
-            fail("should not be able to insert null key");
-        } 
-        catch (IllegalNullKeyException e) { /* expected */ } 
-        catch (Exception e) {
-            fail("insert null key should not throw exception "+e.getClass().getName());
-        }
-    }
-    
-    // TODO add your own tests of your implementation
-    
+  }
 }

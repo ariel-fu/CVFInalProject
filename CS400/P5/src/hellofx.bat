7825cdc71b197647704f9063bb028cdb6809fbb6
alvickyfun <al.vicky.fun@gmail.com>
2020-04-09 20:54:03 -0500

    ewf

--- CS400/P5/src/hellofx.bat
+++ CS400/P5/src/hellofx.bat
@@ -1,4 +1,4 @@
-"C:\Program Files\Java\jdk-11.0.6\bin\javaw.exe" 
+C:\Program Files\Java\jdk-11.0.6\bin\javaw.exe 
 --module-path "C:\Users\Ariel\javafx-sdk-11.0.2\lib" 
 --add-modules javafx.controls,javafx.fxml -Dfile.encoding=Cp1252 
 -classpath "C:\Users\Ariel\eclipse-workspace\Ariel\CS400\P5\HelloFX" application.Main

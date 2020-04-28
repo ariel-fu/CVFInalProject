3efe58535e5061f21f085678dd056c122e9d3f29
ariel <ariel@royal-03.cs.wisc.edu>
2020-04-28 12:05:10 -0500

    try to change change button

---
+++ home_page.js
@@ -0,0 +1,10 @@
+function change(){
+    var elem = document.getElementById("change");
+if (elem.value=="CHANGE"){
+	 elem.innerHTML = 'Doggos are the best';
+ } else if(elm.value == 'Doggos are the best'){
+ 	 elem.innerHTML = 'Yes they are :)';
+ } else {
+ 	 elem.innerHTML = 'CHANGE';
+}
+}

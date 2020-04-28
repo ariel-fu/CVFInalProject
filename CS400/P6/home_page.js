df1205bd4bb230eab1a470a4feef6741506fb87b
ariel-fu <60861741+ariel-fu@users.noreply.github.com>
2020-04-28 13:35:49 -0500

    Update home_page.js

--- CS400/P6/home_page.js
+++ CS400/P6/home_page.js
@@ -1,4 +1,13 @@
 function change(){
-	document.getElementById("change").innerHTML = 'Doggos are the best!';
+    var elem = document.getElementById("change");
 	
+ var stringInDoc = document.getElementById("change").innerHTML;
+if (stringInDoc==="CHANGE"){
+document.getElementById("change").innerHTML = 'Doggos are the best!';
+ } else if(stringInDoc===("Doggos are the best!")){
+ 	 document.getElementById("change").innerHTML = 'Puppies are adorable!';
+
+ } else {
+ 	  document.getElementById("change").innerHTML = 'CHANGE';
+}
 }

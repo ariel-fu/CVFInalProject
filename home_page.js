efd734abe4eec00fb7c8239735166e241460021a
ariel <ariel@royal-03.cs.wisc.edu>
2020-04-28 12:40:15 -0500

    modified:   home_page.js

--- home_page.js
+++ home_page.js
@@ -1,12 +1,14 @@
 function change(){
     var elem = document.getElementById("change");
-if (elem.value=="CHANGE"){
-	 document.getElementById("change").innerHTML = "Doggos are the best!";
 
- } else if(elm.value == "Doggos are the best"){
- 	 document.getElementById("change").innerHTML = "Puppies are adorable!";
+ document.getElementById("change").innerHTML = 'Doggos are the best!';
+if (elem.value=='CHANGE'x){
+	 document.getElementById("change").innerHTML = 'Doggos are the best!';
+
+ } else if(elm.value == 'Doggos are the best'){
+ 	 document.getElementById("change").innerHTML = 'Puppies are adorable!';
 
  } else {
- 	  document.getElementById("change").innerHTML = "CHANGE";
+ 	  document.getElementById("change").innerHTML = 'CHANGE';
 }
 }

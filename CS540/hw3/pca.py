0984d74236634d7abebd025040d8ef2ec5dcc058
alvickyfun <al.vicky.fun@gmail.com>
2021-10-01 11:40:54 -0500

    having issues with values perc, i think

--- CS540/hw3/pca.py
+++ CS540/hw3/pca.py
@@ -16,8 +16,11 @@ def load_and_center_dataset(filename):
 
 def get_covariance(dataset):
     # TODO: add your code here
-    transSet = numpy.transpose(dataset)
-    return numpy.dot(transSet, dataset)
+    s = dataset
+    n = len(s)
+    s = numpy.dot(numpy.transpose(s), s)
+    s = s/(n-1)
+    return s
 
 def get_eig(S, m):
     # TODO: add your code here
@@ -25,16 +28,19 @@ def get_eig(S, m):
     # rangeEigenObjects = tuple(m)
     # eigenvaluesEighReturn, eigenvectorsEighReturn = eigh(S, rangeEigenObjects)
 
-    eigenvalues, eigenvectors = eigh(S)
+    length = len(S)
+    eigenvalues, eigenvectors = eigh(S, subset_by_index=[length-m, length-1])
+
+    # eigenvalues, eigenvectors = eigh(S)
     # print("eigenvalues; ", eigenvaluesEighReturn)
     # print("eigenvectors; ", eigenvectorsEighReturn)
 
     # reverse the order of both eigenvalues and eigenvectors
-    # eigenvalues = numpy.flipud(eigenvalues)
+    eigenvalues = numpy.flipud(eigenvalues)
     # eigenvalues = eigenvalues[:m].copy()
     # print(eigenvalues)
     # # print("values: ", eigenvaluesEighReturn)
-    # eigenvectors = numpy.flipud(eigenvectors)
+    # eigenvectors = numpy.fliplr(eigenvectors)
     # eigenvectors = eigenvectors[:m].copy()
     # print(eigenvectors)
     # print("vectors: ", eigenvectorsEighReturn)
@@ -43,18 +49,24 @@ def get_eig(S, m):
 
     # create a diagonal array with the eigenvalues on the diagonal
     valueArray = numpy.diag(eigenvalues)
-    print(valueArray)
+    # print(valueArray)
     # slice 
     # print("eig value: ", valueArray)
 
     # create a Numpy array to hold the eigenvectors corresponding to the eigenvalues in the column
     vectorArray = None
+
+    # transpose the eigenvectors to get them as rows, not columns
+    # print(eigenvectors)
+    # print("-------")
+    eigenvectors = eigenvectors.T
+    # print("transposed: ", eigenvectors)
+    # print("-------")
     # print("vector array: ", vectorArray)
 
     # loop through all the eigenvalues and add them to the numpy arrays
     isFirst = True
     for eigenvector in eigenvectors:
-
         # eigenvector is a row vector, transpose and concat the curr vectorArray to the eigenvector
         # print("eig vector: ", eigenvector)
         # eigenvector = numpy.transpose(eigenvector)
@@ -80,7 +92,38 @@ def get_eig(S, m):
 
 def get_eig_perc(S, perc):
 # TODO: add your code here
-    print("get eig perc")
+    # get all the eignvalues and eigenvectors
+    eigenvalues, eigenvectors = eigh(S)
+    # set the return values and currEigenvalueSum
+    valuesPerc = None
+    vectorsPerc = None
+    currEigenvalueSum = 0
+
+    # for every eigenvalue, check if the variance explained is bigger than perc
+    for (eigenvalue, eigenvector) in zip(eigenvalues, eigenvectors):
+
+        # get the variance explained
+        currEigenvalueSum += eigenvalue
+        print(eigenvalue)
+        currExplainedPerc = (eigenvalue/currEigenvalueSum)
+        print(currExplainedPerc)
+
+        # transpose the eigenvector
+        eigenvector = eigenvector.reshape(-1, 1)
+        # if the explainedPerc is bigger than the perc, add to the ret values
+        if((currExplainedPerc) > perc):
+            if(type(valuesPerc) == None):
+                valuesPerc = numpy.array([eigenvalue])
+                vectorsPerc = eigenvector
+            else:
+                valuesPerc = numpy.concatenate((valuesPerc, numpy.array([eigenvalue])))
+                vectorsPerc = numpy.concatenate((eigenvector, vectorsPerc), axis=1)
+    
+    # reverse the order to be in descending order
+    eigenvalues = numpy.flipud(eigenvalues)
+    # add to a diagonal matrix
+    valuesPerc = numpy.diag(eigenvalues)
+    return valuesPerc, vectorsPerc
 
 
 def project_image(img, U):

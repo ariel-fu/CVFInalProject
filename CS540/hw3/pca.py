deb184d9cfea3334632f21e83d2550c75d42a8b8
alvickyfun <al.vicky.fun@gmail.com>
2021-10-01 23:52:43 -0500

    needs testing

--- CS540/hw3/pca.py
+++ CS540/hw3/pca.py
@@ -3,7 +3,7 @@ import os
 import numpy
 
 # import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 def load_and_center_dataset(filename):
@@ -94,43 +94,115 @@ def get_eig_perc(S, perc):
 # TODO: add your code here
     # get all the eignvalues and eigenvectors
     eigenvalues, eigenvectors = eigh(S)
-    # set the return values and currEigenvalueSum
-    valuesPerc = None
-    vectorsPerc = None
-    currEigenvalueSum = 0
 
+    # calculate the total sum of eigenvalues
+    totEigenvalueSum = 0
+    for eigenvalue in eigenvalues:
+        totEigenvalueSum += eigenvalue
+    
+    numEigenObjects = 0
     # for every eigenvalue, check if the variance explained is bigger than perc
     for (eigenvalue, eigenvector) in zip(eigenvalues, eigenvectors):
 
         # get the variance explained
-        currEigenvalueSum += eigenvalue
-        print(eigenvalue)
-        currExplainedPerc = (eigenvalue/currEigenvalueSum)
-        print(currExplainedPerc)
+        currExplainedPerc = (eigenvalue/totEigenvalueSum)
+        # print("curr eigen val: ", eigenvalue)
+        # print("curr sum: ", currEigenvalueSum)        
+        # print("curr perc: ", currExplainedPerc)
+        # print("--------------")
 
-        # transpose the eigenvector
-        eigenvector = eigenvector.reshape(-1, 1)
-        # if the explainedPerc is bigger than the perc, add to the ret values
+        # if the explainedPerc is bigger than the perc, add 1 to the number of eigenobjects to retrieve
         if((currExplainedPerc) > perc):
-            if(type(valuesPerc) == None):
-                valuesPerc = numpy.array([eigenvalue])
-                vectorsPerc = eigenvector
-            else:
-                valuesPerc = numpy.concatenate((valuesPerc, numpy.array([eigenvalue])))
-                vectorsPerc = numpy.concatenate((eigenvector, vectorsPerc), axis=1)
+            numEigenObjects+=1
+
+            # print("curr explained perc: ", currExplainedPerc)
+            # print("per: ", perc)
+            # print("-====================-")
+            # transpose the eigenvector
+
+            # print("pretransposed: ", eigenvector)
+            # eigenvector = eigenvector.reshape(-1, 1)
+            # print("transposed: ", eigenvector)
+            # print("==============")
+            # if(isFirst):
+            #     valuesPerc = numpy.array([eigenvalue])
+            #     vectorsPerc = eigenvector
+            #     isFirst = False
+            # else:
+            #     valuesPerc = numpy.concatenate((valuesPerc, numpy.array([eigenvalue])))
+            #     vectorsPerc = numpy.concatenate((eigenvector, vectorsPerc), axis=1)
+            # print(valuesPerc)
     
     # reverse the order to be in descending order
-    eigenvalues = numpy.flipud(eigenvalues)
-    # add to a diagonal matrix
-    valuesPerc = numpy.diag(eigenvalues)
+    # eigenvalues = numpy.flipud(valuesPerc)
+    # # add to a diagonal matrix
+    # valuesPerc = numpy.diag(eigenvalues)
+    valuesPerc, vectorsPerc = get_eig(S, numEigenObjects)
+    # print(valuesPerc)
     return valuesPerc, vectorsPerc
+
 
 
 def project_image(img, U):
 # TODO: add your code here
-    print("project image")
+    # get alpha = (U^T)*dot*(img)
+    # transU = numpy.transpose(U)
+    # alpha = numpy.dot(transU, img)
+    # # dot alpha with the U matrix
+    # return numpy.dot(alpha, numpy.transpose(U))
+
+    # keep track of the sum
+    summation = None
+    U = numpy.transpose(U)
+    isFirst = True
+    alphaList = numpy.dot(U, img)
+    for alpha, u in zip(alphaList, U):
+        if(isFirst):
+            summation = alpha*u
+            isFirst = False
+        else:
+            summation += alpha*u
+    # for u in U:
+    #     # print("beofre transp: ", u)
+    #     # u = u.reshape(-1, 1)
+    #     # img = img.reshape(-1, 1)
+    #     # print("after transp: ", u)
+    #     alpha = numpy.dot(u, img)
+    #     print(alpha)
+    #     print(u)
+    #     summation += alpha
+
+    return summation
+
 
 
 def display_image(orig, proj):
 # TODO: add your code here
-    print("display image")
+    # reshape orig and proj to be 32 x 32
+    orig = orig.reshape(32, 32)
+    proj = proj.reshape(32, 32)
+
+    orig = orig.transpose()
+    proj = proj.transpose()
+    # create a figure with one row of two subplots
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
+    # set the titles
+    ax1.set_title('Original')
+    ax2.set_title('Projection')
+
+
+    # get the ratio to make the colorbar the same height as the plots
+    # ratio1 = (0.047)*(ax1.bbox.height/ax1.bbox.width)
+    # imshow() to render orig image in 1st subplot and proj in 2nd subplot
+    origShow = ax1.imshow(orig, aspect='equal')
+    fig.colorbar(origShow, ax=ax1)
+    # fig.colorbar(origShow, ax=ax1, fraction=ratio1)
+
+    # ratio2 = (0.047)*(ax2.bbox.height/ax2.bbox.width)
+    projShow = ax2.imshow(proj, aspect='equal')    
+    fig.colorbar(projShow, ax=ax2)
+    # fig.colorbar(projShow, ax=ax2,fraction=ratio2)
+    # render the plots
+    plt.show()
+
+

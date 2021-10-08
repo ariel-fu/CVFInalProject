5f61b4117cfb2863f6f22d51f5462a5a04fb0a60
alvickyfun <al.vicky.fun@gmail.com>
2021-10-08 13:24:25 -0500

    working version - but slow

--- CS540/hw4/pokemon_stats.py
+++ CS540/hw4/pokemon_stats.py
@@ -17,6 +17,7 @@ def load_data(filepath):
             pokemonDict = {}
             row = list(row)
             # break if we have 20 pokemon
+            # TESTER
             if(counter >= 20):
                 break
 
@@ -53,110 +54,201 @@ def calculate_x_y(stats):
     # x = Attack + Sp. Atk + Speed
     x = stats['Attack'] + stats['Sp. Atk'] + stats['Speed']
     # y = Defense + Sp. Def + HP
-    y = stats['Defense'] + stats['Sp. Def'] + stats['Speed']
+    y = stats['Defense'] + stats['Sp. Def'] + stats['HP']
     # couple x and y into a tuple and return it
     ret = (x, y)
     return ret
 
+# helper class of Clusters
+class Cluster:
+    def __init__(cluster, index, items, index1, index2, eucDistance, size):
+        cluster.index = index
+        cluster.items = items
+        cluster.index1 = index1
+        cluster.index2 = index2
+        cluster.eucDistance = eucDistance
+        cluster.size = size
 
 
 def hac(dataset):
     """ performs single linkage hierarchical agglomerative clustering on the Pokemon with the (x,y) feature representation, and returns a data structure representing the clustering."""
     clusters = []
-    listEucDistance = []
     hacList = []
     # place each element in the data set into its own cluster and number them appropriately
     index = 0
     for data in dataset:
+        # Cluster = (current index, items, smaller index, larger index, distance between clusters, and total size)
+        currCluster = Cluster(index, [data], index, index, 0, 1)
         if index == 0:
             # set the initial clusters to the first data set
-            clusters = [(data, index)]
+            clusters = [currCluster]
         else:
             # append each data as a new cluster
-            clusters = [clusters, (data, index)]
+            clusters = clusters + [currCluster]
         # increment the counter
         index += 1
-
     # while there are more than 1 clusters left
-    while(len(clusters) > 0):
+    listEucDistance = []
+    listDistance = []
+    clusterCounter = len(clusters)
+    while(clusterCounter > 1):
         i = 0
         # for all current clusters:
         while(i < len(clusters)):
-            j = i
+            # get a counter and the list to hold possible clusters
+            j = i+1
             while(j < len(clusters)):
                 # get the current clusters and their corresponding indices
                 cluster1 = clusters[i]
-                index1 = cluster1[1]
                 cluster2 = clusters[j]
-                index2 = cluster2[1]
-                # find the smallest euclidean distance between two current clusters
-                dist = single_linkage(cluster1, cluster2)
-                newEucDist = [index1, index2, dist]
-                # add to the list of euclidean distance: [i, j, dist, num items]
-                if(len(listEucDistance) == 0):
-                    listEucDistance = [newEucDist]
-                else:
-                    listEucDistance = [listEucDistance, newEucDist]
+                if(not(cluster1 == None or cluster2 == None)): 
+                    # print("cluster2")
+                    # print(cluster2.items)
+                    index1 = cluster1.index
+                    index2 = cluster2.index
                 
+                    # find the smallest euclidean distance between two current clusters
+                    dist = single_linkage(cluster1, cluster2)
+                
+                    # Cluster = (current index, items, smaller index, larger index, distance between clusters, and total size)
+                    newEucDist = Cluster(index, (cluster1.items + cluster2.items), index1, index2, dist, cluster1.size + cluster2.size)               
+                    # add to the list of euclidean distance: [i, j, dist, num items]
+                    if(len(listEucDistance) == 0):
+                        listEucDistance = [newEucDist]
+                    else:
+                        listEucDistance = listEucDistance+[newEucDist]
+                
+                # increment the counter
+                j += 1
+            i += 1
+            
         # find the smallest euclidean distance
-        smallest = listEucDistance[0][2]
+        smallestCluster = listEucDistance[0]
         for listItem in listEucDistance:
-            currDist = listItem[2]
-            if(currDist < smallest):
-                smallest = currDist
-        # add the information to the return cluster array (index1, index2, and euclidean distance)
-        if(len(hacList) == 0):
-            hacList = [smallest]
-        else:
-            hacList = [hacList, smallest]
-        # add the new cluster to the list of clusters with index = index+1
+            currDist = listItem.eucDistance
+            if(currDist < smallestCluster.eucDistance):
+                smallestCluster = listItem
         
-        # TODO:
-        # create a new cluster by merging the values from index1 and index2 into an [(index1), (index2)]
-        # add to a new index
-        merge = []
-        # ARIEL: THIS IS BROKEN!
-        clusters = clusters[clusters, (smallest, index)]
+        # add the new cluster to the list of clusters and the hacArray
+        clusters = (clusters + [smallestCluster])
+        clusterCounter += 1
+        if(len(hacList) == 0):
+            hacList = [smallestCluster]
+        else:
+            hacList = hacList+[smallestCluster]
         index += 1
 
-
-        # get the two indices of the elements merged
-        index1 = smallest[0]
-        index2 = smallest[1]
         # remove the elements at the two indices
-        clusters.pop(index1)
-        clusters.pop(index2)
+        clusters[smallestCluster.index1] = None
+        clusters[smallestCluster.index2] = None
+        clusterCounter -= 2
+        # restart the distance array
+        listEucDistance = []
+    
     
     # convert hacList into a numpy array
     counter = 0
-    for cluster in clusters:
-        # get the total number of items
-        print("nothing for now")
+
+# import numpy as np
+# arr = np.array([[1,2,3],[4,5,6]])
+# row = np.array([7,8,9])
+# arr = np.r_[arr,[row]]
+# print(arr)
+    retNumpyArr = []
+    for cluster in hacList:
+        # row = [index1, index2, euc distance, # of items]
+        index1 = cluster.index1
+        index2 = cluster.index2
+        eucDistance = cluster.eucDistance
+        numItems = cluster.size
+
+        row = np.array([index1, index2, eucDistance, numItems])
+        print(row)
+        print("items:")
+        print(cluster.items)
+        print("------------------------------")
+        if(len(retNumpyArr) == 0):
+            retNumpyArr = row
+        else:
+            retNumpyArr=np.vstack((retNumpyArr, row))
+
+        # print(cluster)
     # return numpy array
-    return None
-# Helper method to return the euc distance between two clusters
+    return retNumpyArr
+
+
+# Helper method to return the smallest euc distance between two clusters
 def single_linkage(cluster1, cluster2):
     # check for invalidity  - nan/inf
-    if(not(validate_cluster(cluster1) or validate_cluster(cluster2))):
+    if(not(validate_cluster(cluster1) or not(validate_cluster(cluster2)))):
         return None
     # get the points
-    point1 = cluster1[0][0]    
-    point2 = cluster2[0][0]        
+    point1 = cluster1.items    
+    point2 = cluster2.items        
     # calculate the distance between them
-    return math.dist(point1, point2)
+    dist = []
+    for p1 in point1:
+        for p2 in point2:
+            if(len(dist) == 0):
+                dist = [math.dist(p1, p2)]
+            else:
+                dist = dist + [math.dist(p1, p2)]
+    # find the smallest euclidien distance
+    smallest = dist[0]
+    for eucDist in dist:
+        if(eucDist < smallest):
+            smallest = eucDist
+
+    return smallest
+
 
 # Helper method to determine whether the cluster is valid or not
 def validate_cluster(cluster):
-    point1 = cluster[0][0]
-    x1 = point1[0]
-    y1 = point1[1]
-    # check for invalidity  - nan/inf
-    if(math.isnan(x1) or math.isinf(x1) or math.isnan(y1) or math.isinf(y1)):
-        return False
-    else:
-        return True
+    pointSet = cluster.items
+    for point in pointSet:
+        x = point[0]
+        y = point[1]
+        # check for invalidity  - nan/inf
+        if(math.isnan(x) or math.isinf(x) or math.isnan(y) or math.isinf(y)):
+            return False
+    return True
 
 def random_x_y(m): 
     """ takes in the number of samples we want to randomly generate, and returns these samples in a single structure.""" 
+
 def imshow_hac(dataset): 
     """performs single linkage hierarchical agglomerative clustering on the Pokemon with the (x,y) feature representation, and imshow the clustering process."""
+
+
+## TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+## REMOVE!
+
+def main():
+    pokemons = load_data("Pokemon.csv")
+    pokemons[1]
+    calculate_x_y(pokemons[1])
+    pokemons_x_y = []
+    print(pokemons[7])
+    print(calculate_x_y(pokemons[7]))
+    pokemons_x_y = []
+    for row in pokemons:
+        pokemons_x_y.append(calculate_x_y(row))
+
+    print(hac(pokemons_x_y))
+
+    # pokemons_x_y.append((1, 1))
+    # pokemons_x_y.append((2, 1))
+    # pokemons_x_y.append((2, 4))
+    # pokemons_x_y.append((5, 5))
+    # pokemons_x_y.append((3, 2))
+
+    # pokemons_x_y = []
+    # pokemons_x_y.append(calculate_x_y(pokemons[1]))
+    # pokemons_x_y.append(calculate_x_y(pokemons[2]))
+    # pokemons_x_y.append(calculate_x_y(pokemons[3]))
+    # pokemons_x_y.append(calculate_x_y(pokemons[4]))
+    
+    print(hac(pokemons_x_y))
+
+if __name__ =="__main__":
+    main()

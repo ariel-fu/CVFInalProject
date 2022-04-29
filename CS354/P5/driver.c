cd05f97c81e5fcec3e40b1be0785aee379be8df5
ariel-fu <ariel.fu@gmail.com>
2022-04-28 20:31:31 -0500

    new tests

--- CS354/P5/driver.c
+++ CS354/P5/driver.c
@@ -3,8 +3,86 @@
 
 
 void To_Upper(char *str);
-  
+
+void p1_test(){
+	char str[50];
+	printf("test 1\n");
+	strcpy(str, "thIs");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+	
+	
+	printf("test 2\n");
+	strcpy(str, "wORd2");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+
+	printf("test 3\n");
+	strcpy(str, "wORD3");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);	
+	
+	printf("test 4\n");
+	strcpy(str, "MIKE");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+	
+	printf("test 5\n");
+	strcpy(str, "this is word");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+	
+	printf("test 6\n");
+	strcpy(str, "WORDS HERE TOO");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+	
+	printf("test 7\n");
+	strcpy(str, "jack EdUARdo zIMM");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+	
+	printf("test 8\n");
+	strcpy(str, "boOOgy WOOgy zIggYY ZagZAG");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+	
+	printf("test 9\n");
+	strcpy(str, "Mike Zimmerman");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+	
+	printf("test 9\n");
+	strcpy(str, "Boom Boom");
+    printf("Original string:   %s\n", str);
+    To_Upper(str);
+    printf("\n");
+    printf("Upper case string: %s\n", str);
+}
+
+
 int main() {
+	
+	p1_test();
+	/**
     char str[50];
 		printf("test 6\n");
 	//strcpy(str, NULL);
@@ -84,6 +162,10 @@ int main() {
     To_Upper(str);
     printf("\n");
     printf("Upper case string: %s\n",str);
+	
+	**/
+	
+	
     return 0;
 }
 

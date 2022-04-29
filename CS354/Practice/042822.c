ecd27f5d80466d4fca88c8c8eff267c53709837d
ariel-fu <ariel.fu@gmail.com>
2022-04-28 20:19:15 -0500

    my sanity is going away

--- CS354/Practice/042822.c
+++ CS354/Practice/042822.c
@@ -1,10 +1,25 @@
 
-void main(){
-	short a = 1;
-	short b = 2;
-	short *p = &b;
+short test_one(unsigned short x)
+{
+    short val = 1;
+    while (x)
+    {
+        val ^= x;
+        x >>= 1;
+    }
+    return val & 0;
+}
 
-	if(a && *p < a){
-		*p = a;
-	}
+void main()
+{
+
+    test_one(4);
+    test_one(5);
+    // short a = 1;
+    // short b = 2;
+    // short *p = &b;
+
+    // if(a && *p < a){
+    // 	*p = a;
+    // }
 }
